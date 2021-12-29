83f2ecbb0ed09132961a14db0018b8e3e0c3f4df
Peter Sarll <petersarll@gmail.com>
2021-12-28 20:40:31 -0800

    adding new smart contract to contracts

---
+++ src/contracts/Token.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.5.0;
+
+contract Token {
+    string public name = "Token";
+    string public symbol = "LCA";
+    string public standard = "Token v1.0.0"
+    uint256 public totalSupply;
+
+    
+}
