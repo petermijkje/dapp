2f8806ab2139d3b2234555d2fe7e8560f9851b30
Peter Sarll <petersarll@gmail.com>
2021-12-28 22:10:18 -0800

    adding abis

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,10 +1,12 @@
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 contract Token {
+    
     string public name = "Token";
     string public symbol = "LCA";
-    string public standard = "Token v1.0.0"
+    string public standard = "Token v1.0.0";
     uint256 public totalSupply;
 
-    
+
 }
--- truffle-config.js
+++ truffle-config.js
@@ -39,6 +39,11 @@ module.exports = {
    */
 
   networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*"
+    },
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
     // You should run a client (like ganache-cli, geth or parity) in a separate terminal
@@ -76,7 +81,7 @@ module.exports = {
     // production: true    // Treats this network as if it was a public net. (default: false)
     // }
   },
-  contracts_directory: './src/contratcs/',
+  contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
 
   // Set default mocha options here, use special reporters etc.
