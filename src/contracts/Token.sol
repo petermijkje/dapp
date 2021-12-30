2e02a99cb3989f52c14e0d1c3e6f6f1ba70b1855
Peter Sarll <petersarll@gmail.com>
2021-12-29 18:49:02 -0800

    add testing

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -2,11 +2,14 @@
 pragma solidity ^0.8.0;
 
 contract Token {
-    
+
     string public name = "Token";
     string public symbol = "LCA";
     string public standard = "Token v1.0.0";
+    uint256 public decimals = 18;
     uint256 public totalSupply;
 
-
+    constructor() public {
+        totalSupply = 1000000 * (10 ** decimals);
+    }
 }
---
+++ test/Token.test.js
@@ -0,0 +1,47 @@
+const Token = artifacts.require('./Token');
+
+require('chai')
+    .use(require('chai-as-promised'))
+    .should()
+
+contract('Token', (accounts) => {
+    let token 
+    const name = "Token"
+    const symbol = "LCA"
+    const decimals = '18'
+    const totalSupply = '1000000000000000000000000'
+    const standard = "Token v1.0.0"
+
+
+    beforeEach(async () => {
+        token = await Token.new()
+    })
+
+    describe('deployment', () => {
+        it('tracks the name', async () => {
+            // read token and check it
+            const result = await token.name();
+            result.should.equal(name)
+        })
+
+        it('tracks the symbol', async () => {
+            const result = await token.symbol()
+            result.should.equal(symbol)
+        })
+
+        it('returns the correct decimal number', async () => {
+            const result = await token.decimals()
+            result.toString().should.equal(decimals)
+        })
+
+        it('it should return the correct total supply', async () => {
+            const result = await token.totalSupply()
+            result.toString().should.equal(totalSupply)
+        })
+        
+        it('returns the correct token version', async () => {
+            const result = await token.standard()
+            result.should.equal(standard)
+        })
+    })
+})
