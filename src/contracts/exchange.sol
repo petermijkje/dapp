7778c89283c0aa527aad4fb99db7e0d5bf628502
Peter Sarll <petersarll@gmail.com>
2022-01-23 14:16:17 -0800

    Create a Deposit Ether function and Test
    Create a deposit ether function that allows users to deposit ether and in the depositFrom function, prevent users from depositing ether. test the function in exchange.test.js

--- src/contracts/exchange.sol
+++ src/contracts/exchange.sol
@@ -14,26 +14,30 @@ contract Exchange {
     string public standard = "Token v1.0.0";
     uint256 public decimals = 18;
     uint256 public totalSupply;
-    uint256 public feePercent;
 
-    address public feeAccount; //the account that receives exchange fees
-    //track balances
+    uint256 public feePercent;
+    address public feeAccount;
+
+    address constant ETHER = address(0); //store ether in tokens mapping with blank address
     mapping(address => mapping(address => uint256)) public tokens;
+
+    event Deposit(address token, address user, uint256 amount, uint256 balance);
 
     constructor(address _feeAccount, uint256 _feePercent) public {
         feeAccount = _feeAccount;
         feePercent = _feePercent;
     }
 
-    function depositToken(address _token, uint256 _amount) public {
-        // which token?
-        // how much?
-        // end tokens to this contract
-        //manage deposit
-        //emit event
+    function depositEther() payable public {
+        tokens[ETHER][msg.sender] = tokens[ETHER][msg.sender].add(msg.value);
+        emit Deposit(ETHER, msg.sender, msg.value, tokens[ETHER][msg.sender]);
+    }
 
+    function depositToken(address _token, uint256 _amount) public {
+        require(_token != ETHER);
         require(Token(_token).transferFrom(msg.sender, address(this), _amount));
         tokens[_token][msg.sender] = tokens[_token][msg.sender].add(_amount);
+        emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
     }
 
 }
--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -59,12 +59,13 @@ contract("Exchange", ([deployer, feeAccount, user1]) => {
     let result;
     let amount;
 
-    beforeEach(async () => {
-      amount = tokens(10)
-      await token.approve(exchange.address, amount, {from: user1})
-      const result = await exchange.depositToken(token.address, amount, {from: user1})
-    })
     describe("Success", () => {
+      beforeEach(async () => {
+        amount = tokens(10)
+        await token.approve(exchange.address, amount, {from: user1})
+        result = await exchange.depositToken(token.address, amount, {from: user1})
+      })
+
       it("tracks the token deposit", async () => {
           //check token balance
           let balance 
@@ -74,10 +75,31 @@ contract("Exchange", ([deployer, feeAccount, user1]) => {
           balance.toString().should.equal(amount.toString())
 
       })
+
+      it('emits a Deposit event', () => {
+        const log = result.logs[0]
+        log.event.should.eq('Deposit')
+        const event = log.args
+        event.token.should.equal(token.address, "token address is correct")
+        event.user.should.equal(user1, "user address is correct")
+        event.amount.toString().should.equal(amount.toString(), "amount is correct")
+        event.balance.toString().should.equal(amount.toString(), "balance is correct")
+      })
     })
 
     describe("Failure", () => {
 
+      beforeEach(async () => {
+        amount = tokens(10)
+      })
+      it("rejects ether deposits", async () => {
+        await exchange.depositToken('0x0000000000000000000000000000000000000000', tokens(10), {from: user1}).should.be.rejectedWith(EVM_REVERT)
+      })
+      
+      it("fails when no deposits are approved", async () => {
+        // no amount is approved prior to depositing tokens
+        await exchange.depositToken(token.address, amount, {from: user1}).should.be.rejectedWith(EVM_REVERT)
+      })
     })
   })
 });
