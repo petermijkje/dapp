0b77f35afb20d60db869fff216626ea43eb2fbd0
Peter Sarll <petersarll@gmail.com>
2022-02-16 21:47:20 -0800

    Seed exchange
    seed the exchange on ganache and start creating a test script

---
+++ scripts/seed-exchange.js
@@ -0,0 +1,11 @@
+const Token = artifacts.require("Token");
+const Exchange = artifacts.require("Exchange");
+
+module.exports = async function(callback) {
+try {
+
+} catch(err) {
+    console.log(err)
+}
+callback()
+}
