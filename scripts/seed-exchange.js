30e0b7b39f79106447f0b66c7db24d4df759b0b5
Peter Sarll <petersarll@gmail.com>
2022-02-17 22:07:55 -0800

    Add seed exchange test
    Create a seed exchange test that allows the tokens and exchange to be fetched

--- scripts/seed-exchange.js
+++ scripts/seed-exchange.js
@@ -4,6 +4,14 @@ const Exchange = artifacts.require("Exchange");
 module.exports = async function(callback) {
 try {
 
+    const acounts = await web3.eth.getAccounts()
+
+    const token = await Token.deployed()
+    console.log("token fetched", token.address)
+
+    const Exchange = await Exchange.deployed()
+    console.log("Exchange fetched", exchange.address)
+
 } catch(err) {
     console.log(err)
 }
