5a91ba560c25983a437b0a6c87dafe57ef421d28
Peter Sarll <petersarll@gmail.com>
2022-02-18 20:59:39 -0800

    add seed exchange
    create a transaction on the exchange to test

--- scripts/seed-exchange.js
+++ scripts/seed-exchange.js
@@ -1,19 +1,26 @@
 const Token = artifacts.require("Token");
 const Exchange = artifacts.require("Exchange");
 
-module.exports = async function(callback) {
-try {
+module.exports = async function (callback) {
+  try {
+    const acounts = await web3.eth.getAccounts();
 
-    const acounts = await web3.eth.getAccounts()
+    const token = await Token.deployed();
+    console.log("token fetched", token.address);
 
-    const token = await Token.deployed()
-    console.log("token fetched", token.address)
+    const Exchange = await Exchange.deployed();
+    console.log("Exchange fetched", exchange.address);
 
-    const Exchange = await Exchange.deployed()
-    console.log("Exchange fetched", exchange.address)
+    const sender = accounts[0];
+    const receiver = accounts[1];
 
-} catch(err) {
-    console.log(err)
-}
-callback()
-}
+    let amount = eb3.utils.toWei("10000", "ether");
+
+    await token.transfer(receiver, amount, { from: sender });
+    console.log(`Transferred ${amount} tokens from ${sender} to ${receiver}`);
+    
+  } catch (err) {
+    console.log(err);
+  }
+  callback();
+};
