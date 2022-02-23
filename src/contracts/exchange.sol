631ebdb11f42bac06d09349729d674fdc2bd90e8
Peter Sarll <petersarll@gmail.com>
2022-02-22 22:54:58 -0800

    Add seed echange

--- src/contracts/exchange.sol
+++ src/contracts/exchange.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import './token.sol';
+import './Token.sol';
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 
@@ -30,35 +30,35 @@ contract Exchange {
     event Deposit(address token, address user, uint256 amount, uint256 balance);
     event Withdraw(address token, address user, uint amount, uint balance);
     event Order(
-        uint id,
+        uint256 id,
         address user,
         address tokenGet,
-        uint amountGet,
+        uint256 amountGet,
         address tokenGive,
-        uint amountGive,
-        uint timestamp
+        uint256 amountGive,
+        uint256 timestamp
     );
     event Cancel(
-        uint id,
+        uint256 id,
         address user,
         address tokenGet,
-        uint amountGet,
+        uint256 amountGet,
         address tokenGive,
-        uint amountGive,
-        uint timestamp
+        uint256 amountGive,
+        uint256 timestamp
     );
     event Trade(
-        uint id,
+        uint256 id,
         address user,
         address tokenGet,
-        uint amountGet,
+        uint256 amountGet,
         address tokenGive,
-        uint amountGive,
+        uint256 amountGive,
         address userFill,
-        uint timestamp
+        uint256 timestamp
     );
 
-    //struct
+    // Structs
     struct _Order {
         uint256 id;
         address user;
@@ -110,7 +110,7 @@ contract Exchange {
         return tokens[_token][_user];
     }
 
-    function makeOrder(address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) public {
+     function makeOrder(address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) public {
         orderCount = orderCount.add(1);
         orders[orderCount] = _Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, block.timestamp);
         emit Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, block.timestamp);
@@ -125,30 +125,25 @@ contract Exchange {
 
     }
 
-    function fillOrder(uint256 _id) public {
-        require(_id > 0 && _id <= orderCount);
-        require(!orderFilled[_id]);
-        require(!orderCancelled[_id]);
+   function fillOrder(uint256 _id) public {
+        require(_id > 0 && _id <= orderCount, 'Error, wrong id');
+        require(!orderFilled[_id], 'Error, order already filled');
+        require(!orderCancelled[_id], 'Error, order already cancelled');
         _Order storage _order = orders[_id];
         _trade(_order.id, _order.user, _order.tokenGet, _order.amountGet, _order.tokenGive, _order.amountGive);
         orderFilled[_order.id] = true;
-        //emit trade event
     }
 
-    function _trade( uint _orderId, address _user, address _tokenGet, uint _amountGet, address _tokenGive, uint _amountGive) internal {
+    function _trade(uint256 _orderId, address _user, address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) internal {
+        // Fee paid by the user that fills the order, a.k.a. msg.sender.
         uint256 _feeAmount = _amountGet.mul(feePercent).div(100);
 
-
-        
         tokens[_tokenGet][msg.sender] = tokens[_tokenGet][msg.sender].sub(_amountGet.add(_feeAmount));
         tokens[_tokenGet][_user] = tokens[_tokenGet][_user].add(_amountGet);
         tokens[_tokenGet][feeAccount] = tokens[_tokenGet][feeAccount].add(_feeAmount);
         tokens[_tokenGive][_user] = tokens[_tokenGive][_user].sub(_amountGive);
-        tokens[_tokenGive][msg.sender] = tokens[_tokenGive][msg.sender].add(_amountGet);
+        tokens[_tokenGive][msg.sender] = tokens[_tokenGive][msg.sender].add(_amountGive);
 
-        //create the trade
-        //charge fees
-        //mark the order as filled
         emit Trade(_orderId, _user, _tokenGet, _amountGet, _tokenGive, _amountGive, msg.sender, block.timestamp);
     }
 }
