f487368032577dc69f8147b23cdf7294c8fc740a
Peter Sarll <petersarll@gmail.com>
2022-02-27 15:41:39 -0800

    creating stylized components
    create stylized components inside of app.js, move the components into one folder and add styling to app.css

--- src/components/Nav/nav.js
+++ src/components/Nav/nav.js
@@ -6,14 +6,14 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 function Nav() {
   return (
-    <div className="nav-bar">
+    <div className="nav-bar bg-dark text-white">
       <Navbar>
           <Container>
-            <Navbar.Brand href="#home">Locacia</Navbar.Brand>
+            <Navbar.Brand href="#home" className="nav-bar bg-dark text-white">Locacia</Navbar.Brand>
             <Navbar.Toggle />
             <Navbar.Collapse className="justify-content-end">
-              <Navbar.Text>
-                Signed in as: <a href="#login">Mark Otto</a>
+              <Navbar.Text className="nav-bar bg-dark text-white">
+                Signed in as: <a href="#login" className="nav-bar bg-dark text-white">Mark Otto</a>
               </Navbar.Text>
             </Navbar.Collapse>
           </Container>
