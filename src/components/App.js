b0670ef79d9639fe5ec9ef9b16f8613c20bb61c0
Peter Sarll <petersarll@gmail.com>
2022-03-02 23:43:15 -0800

    Finding network ID and address
    ensure we are able to import the token and pull out the network id and address

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,44 @@
-import React, { Component,useState, useEffect  } from 'react';
-import './App.css';
-import Web3 from 'web3'
-import Token from '../abis/Token.json'
-import Nav from './Nav/nav.js';
-
+import React, { Component, useState, useEffect } from "react";
+import "./App.css";
+import Web3 from "web3";
+import Token from "../abis/Token.json";
+import Nav from "./Nav/nav.js";
 
 class App extends Component {
   componentDidMount() {
-
-    this.loadBlockchainData()
+    this.loadBlockchainData();
   }
-  
-   async loadBlockchainData() {
-    const web3 = new Web3(window.ethereum)
-    // const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545') 
-    window.ethereum.enable().catch(error => {
-      console.log(error) 
-    })
-   
-    const network = await web3.eth.net.getNetworkType()
-    console.log("network", network)
-    const accounts = await web3.eth.getAccounts()
-    console.log("accounts", accounts)
 
+  async loadBlockchainData() {
+    const web3 = new Web3(window.ethereum);
+    // const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545')
+    window.ethereum.enable().catch((error) => {
+      console.log(error);
+    });
 
+    const network = await web3.eth.net.getNetworkType();
+    const networkID = await web3.eth.net.getId();
+    console.log("network ID", networkID);
+    console.log("network", network);
+
+    const accounts = await web3.eth.getAccounts();
+    console.log("accounts", accounts);
+    console.log("Token", Token);
+
+    const abi = Token.abi;
+    console.log("abi", abi);
+
+    const networks = Token.networks;
+    console.log("network", networks);
+    console.log("Network Data", Token.networks[networkID]);
+    console.log("Address", Token.networks[networkID].address);
   }
 
   render() {
     return (
       <div>
         <Nav />
-        
+
         {/*
         This is originally what the nav bar is but want to find a better one. 
         Keep this until satisfied with the new one
@@ -55,67 +63,84 @@ class App extends Component {
           </div>
         </nav> */}
 
-
         <div className="content">
           <div className="vertical-split">
             <div className="card bg-dark text-white">
-              <div className="card-header">
-                Card Title
-              </div>
+              <div className="card-header">Card Title</div>
               <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="/#" className="card-link">Card link</a>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="/#" className="card-link">
+                  Card link
+                </a>
               </div>
             </div>
             <div className="card bg-dark text-white">
-              <div className="card-header">
-                Card Title
-              </div>
+              <div className="card-header">Card Title</div>
               <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="/#" className="card-link">Card link</a>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="/#" className="card-link">
+                  Card link
+                </a>
               </div>
             </div>
           </div>
           <div className="vertical">
             <div className="card bg-dark text-white">
-              <div className="card-header">
-                Card Title
-              </div>
+              <div className="card-header">Card Title</div>
               <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="/#" className="card-link">Card link</a>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="/#" className="card-link">
+                  Card link
+                </a>
               </div>
             </div>
           </div>
           <div className="vertical-split">
             <div className="card bg-dark text-white">
-              <div className="card-header">
-                Card Title
-              </div>
+              <div className="card-header">Card Title</div>
               <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="/#" className="card-link">Card link</a>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="/#" className="card-link">
+                  Card link
+                </a>
               </div>
             </div>
             <div className="card bg-dark text-white">
-              <div className="card-header">
-                Card Title
-              </div>
+              <div className="card-header">Card Title</div>
               <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="/#" className="card-link">Card link</a>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="/#" className="card-link">
+                  Card link
+                </a>
               </div>
             </div>
           </div>
           <div className="vertical">
             <div className="card bg-dark text-white">
-              <div className="card-header">
-                Card Title
-              </div>
+              <div className="card-header">Card Title</div>
               <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="/#" className="card-link">Card link</a>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="/#" className="card-link">
+                  Card link
+                </a>
               </div>
             </div>
           </div>
