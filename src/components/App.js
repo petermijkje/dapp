a99f516ac190759753213dbb57134e22902aef5e
Peter Sarll <petersarll@gmail.com>
2022-03-06 18:06:08 -0800

    delete extra nav bar
    delete extra nav bar that was in the app.js file

--- src/components/App.js
+++ src/components/App.js
@@ -48,31 +48,6 @@ class App extends Component {
     return (
       <div>
         <Nav />
-
-        {/*
-        This is originally what the nav bar is but want to find a better one. 
-        Keep this until satisfied with the new one
-
-         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-          <a className="navbar-brand" href="/#">Navbar</a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link" href="/#">Link 1</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/#">Link 2</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/#">Link 3</a>
-              </li>
-            </ul>
-          </div>
-        </nav> */}
-
         <div className="content">
           <div className="vertical-split">
             <div className="card bg-dark text-white">
