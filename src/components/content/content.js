82dbf2d18fcc72a7e7f5e9c8ccbeaeb19b999155
Peter Sarll <petersarll@gmail.com>
2022-03-10 21:45:36 -0800

    move content to own component
    move conent to new component inside of components

---
+++ src/components/content/content.js
@@ -0,0 +1,104 @@
+import React, {Component} from "react";
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import 'bootstrap/dist/css/bootstrap.css';
+import {connect} from 'react-redux'
+import {accountSelector} from '../../store/selectors'
+
+class Content extends Component {
+  render(){
+  return (
+    <div>
+<div className="content">
+          <div className="vertical-split">
+            <div className="card bg-dark text-white">
+              <div className="card-header">Card Title</div>
+              <div className="card-body">
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="/#" className="card-link">
+                  Card link
+                </a>
+              </div>
+            </div>
+            <div className="card bg-dark text-white">
+              <div className="card-header">Card Title</div>
+              <div className="card-body">
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="/#" className="card-link">
+                  Card link
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="vertical">
+            <div className="card bg-dark text-white">
+              <div className="card-header">Card Title</div>
+              <div className="card-body">
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="/#" className="card-link">
+                  Card link
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="vertical-split">
+            <div className="card bg-dark text-white">
+              <div className="card-header">Card Title</div>
+              <div className="card-body">
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="/#" className="card-link">
+                  Card link
+                </a>
+              </div>
+            </div>
+            <div className="card bg-dark text-white">
+              <div className="card-header">Card Title</div>
+              <div className="card-body">
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="/#" className="card-link">
+                  Card link
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="vertical">
+            <div className="card bg-dark text-white">
+              <div className="card-header">Card Title</div>
+              <div className="card-body">
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="/#" className="card-link">
+                  Card link
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div> 
+  );
+}
+}
+function mapStateToProps(state) {
+  return {
+    account: accountSelector(state)
+  }
+}
+
+export default connect(mapStateToProps)(Content)
